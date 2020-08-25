7bb25f34f1a3aa851b0db8fd58e4d85f71513a5d
Chand Anand <canand@uber.com>
2020-08-24 23:39:16 -0700

    add parentheses tap dance

--- keymap.c
+++ keymap.c
@@ -12,19 +12,15 @@ enum layers {
 };
 
 enum custom_keycodes {
-#ifdef ORYX_CONFIGURATOR
-  VRSN = EZ_SAFE_RANGE,
-#else
-  VRSN = SAFE_RANGE,
-#endif
-  RGB_SLD
+    CT_LBP,
+    CT_RBP,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |        |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |      ~ |   Q  |   W  |   F  |   P  |   G  |  (   |           |   )  |   J  |   L  |   U  |   Y  |   ;  | \      |
  * |--------+------+------+------+------+------|  [   |           |   ]  |------+------+------+------+------+--------|
@@ -44,14 +40,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_NO,    KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   KC_LEFT,          KC_RGHT,   KC_6,      KC_7,    KC_8,      KC_9,     KC_0,      KC_NO,
-  KC_GRV,   KC_Q,    KC_W,    KC_F,    KC_P,     KC_G,   KC_LBRC,          KC_RBRC,   KC_J,      KC_L,    KC_U,      KC_Y,     KC_SCLN,   KC_BSLS,
-  KC_TAB,   KC_A,    KC_R,    KC_S,    KC_T,     KC_D,                                KC_H,      KC_N,    KC_E,      KC_I,     KC_O,      KC_QUOT,
-  KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_NO,            KC_NO,     KC_K,      KC_M,    KC_COMM,   KC_DOT,   KC_SLSH,   KC_NO,
-  KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_EQL,                                        KC_MINS,   KC_NO,   KC_NO,     KC_NO,    KC_NO,
-                                              KC_LALT,   KC_LGUI,          KC_LALT,   KC_DEL,
+  KC_NO,    KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   KC_LEFT,          KC_RGHT,      KC_6,      KC_7,    KC_8,      KC_9,     KC_0,      KC_NO,
+  KC_GRV,   KC_Q,    KC_W,    KC_F,    KC_P,     KC_G,   TD(CT_LBP),       TD(CT_RBP),   KC_J,      KC_L,    KC_U,      KC_Y,     KC_SCLN,   KC_BSLS,
+  KC_TAB,   KC_A,    KC_R,    KC_S,    KC_T,     KC_D,                                   KC_H,      KC_N,    KC_E,      KC_I,     KC_O,      KC_QUOT,
+  KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_NO,            KC_NO,        KC_K,      KC_M,    KC_COMM,   KC_DOT,   KC_SLSH,   KC_NO,
+  KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_EQL,                                           KC_MINS,   KC_NO,   KC_NO,     KC_NO,    KC_NO,
+                                              KC_LALT,   KC_LGUI,          KC_LALT,      KC_DEL,
                                                         KC_LCTRL,          KC_NO,
-                                        KC_BSPC, KC_LSFT, KC_ESC,          KC_NO,     KC_ENT,    KC_SPC
+                                        KC_BSPC, KC_LSFT, KC_ESC,          KC_NO,        KC_ENT,    KC_SPC
 ),
 /* Keymap 1: Symbol Layer
  *
@@ -76,12 +72,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [SYMB] = LAYOUT_ergodox_pretty(
   // left hand
-  VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,     KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  KC_NO,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,     KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,     KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
   KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,               KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
   KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,     KC_TRNS, KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
   EEP_RST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-                                               RGB_MOD, KC_TRNS,     RGB_TOG, RGB_SLD,
+                                               RGB_MOD, KC_TRNS,     RGB_TOG, KC_NO,
                                                         KC_TRNS,     KC_TRNS,
                                       RGB_VAD, RGB_VAI, KC_TRNS,     KC_TRNS, RGB_HUD, RGB_HUI
 ),
@@ -120,19 +116,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-    switch (keycode) {
-      case VRSN:
-        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-        return false;
-      #ifdef RGBLIGHT_ENABLE
-      case RGB_SLD:
-        rgblight_mode(1);
-        return false;
-      #endif
-    }
+static void
+_td_brackets_finished (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    if (state->keycode == TD(CT_LBP))
+      register_code16 (KC_LBRC);
+    else
+      register_code16 (KC_RBRC);
+  } else if (state->count == 2) {
+    if (state->keycode == TD(CT_LBP))
+      register_code16 (KC_LPRN);
+    else
+      register_code16 (KC_RPRN);
   }
+}
+
+static void
+_td_brackets_reset (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    if (state->keycode == TD(CT_LBP))
+      unregister_code16 (KC_LBRC);
+    else
+      unregister_code16 (KC_RBRC);
+  } else if (state->count == 2) {
+    if (state->keycode == TD(CT_LBP))
+      unregister_code16 (KC_LPRN);
+    else
+      unregister_code16 (KC_RPRN);
+  }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [CT_LBP] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, _td_brackets_finished, _td_brackets_reset),
+    [CT_RBP] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, _td_brackets_finished, _td_brackets_reset)
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
