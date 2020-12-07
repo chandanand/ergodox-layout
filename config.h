fa6c3db1b746081e9e8041e4008b964e0ee9d17f
Chand Anand <canand@uber.com>
2020-12-06 19:51:22 -0800

    corne like layout

--- config.h
+++ config.h
@@ -1,21 +1,14 @@
 #pragma once
 
 // default but used in macros
-#define TAPPING_TERM 200
+#undef TAPPING_TERM
+#define TAPPING_TERM 175
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
-
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD
-
-// Auto Shift and Retro Shift (Auto Shift for Tap Hold).
-#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
-#define RETRO_SHIFT 500
 
 // Recommended for heavy chording.
 #define QMK_KEYS_PER_SCAN 4
 
 // RGB effects on keypresses
 #define RGB_MATRIX_KEYPRESSES
-#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
--- keymap.c
+++ keymap.c
@@ -11,7 +11,7 @@ enum layers {
     FMDIA,
     NMBR,
     SYMB,
-    WIN,
+    FULL
 };
 
 #define XXXX KC_NO
@@ -22,23 +22,6 @@ enum layers {
 #define KC_MAC_COPY LGUI(KC_C)
 #define KC_MAC_PASTE LGUI(KC_V)
 
-#define KC_WIN_FULLSCREEN C(G(KC_F))
-#define KC_WIN_MAXIMIZE A(G(KC_F))
-#define KC_WIN_ALMOST_MAXIMIZE C(A(G(KC_F)))
-#define KC_WIN_CENTER A(G(KC_C))
-#define KC_WIN_SMALLER C(A(S(KC_LEFT)))
-#define KC_WIN_LARGER C(A(S(KC_RGHT)))
-#define KC_WIN_NEXT_DISPLAY C(A(G(KC_RGHT)))
-#define KC_WIN_PREV_DISPLAY C(A(G(KC_LEFT)))
-#define KC_WIN_TOP_LEFT C(G(KC_LEFT))
-#define KC_WIN_TOP_RIGHT C(G(KC_RGHT))
-#define KC_WIN_BOTTOM_LEFT C(S(G(KC_LEFT)))
-#define KC_WIN_BOTTOM_RIGHT C(S(G(KC_RGHT)))
-#define KC_WIN_LEFT_HALF A(G(KC_LEFT))
-#define KC_WIN_RIGHT_HALF A(G(KC_RGHT))
-#define KC_WIN_TOP_HALF A(G(KC_UP))
-#define KC_WIN_BOTTOM_HALF A(G(KC_DOWN))
-
 /* The Keymap */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -48,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------.           ,----------------------------------------------------.
  * |           |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |          |
  * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+----------|
- * |           |   Q  |   W  |   F  |   P  |   G  |      |           |      |   J  |   L  |   U  |   Y  |   '  |          |
+ * |           |   Q  |   W  |   F  |   P  |   G  |      |           |      |   J  |   L  |   U  |   Y  |   ;  |          |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+----------|
  * |           |   A  |   R  |   S  |   T  |   D  |      |           |      |   H  |   N  |   E  |   I  |   O  |          |
  * |           |  CMD |  OPT |  CTL |  SFT |      |------|           |------|      |  SFT |  CTL |  OPT |  CMD |          |
@@ -58,21 +41,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |         |      |      |      | Esc  |                                       |  Del |      |      |      |        |
  *   `-------------------------------------'                                       `------------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        |      |      |       |      | WIN    |
+ *                                        |      |      |       |      |        |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      |      |       | LEAD |        |      |
+ *                                 |      |      |      |       |      |        |      |
  *                                 |Backsp| Tab  |------|       |------|  Enter |Space |
  *                                 | NAVR | FMDIA|      |       |      |   SYMB | NMBR |
  *                                 `--------------------'       `----------------------'
  */
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
-  XXXX,    KC_1,           KC_2,           KC_3,           KC_4,           KC_5,      XXXX,                      XXXX,            KC_6,           KC_7,           KC_8,           KC_9,      KC_0,       XXXX,
-  XXXX,    KC_Q,           KC_W,           KC_F,           KC_P,           KC_G,      XXXX,                      XXXX,            KC_J,           KC_L,           KC_U,           KC_Y,      KC_QUOT,    XXXX,
-  XXXX,    CMD_T(KC_A),    OPT_T(KC_R),    CTL_T(KC_S),    SFT_T(KC_T),    KC_D,      KC_H,                      SFT_T(KC_N),     CTL_T(KC_E),    OPT_T(KC_I),    CMD_T(KC_O),    XXXX,
+  XXXX,    KC_1,           KC_2,           KC_3,           KC_4,           KC_5,      TG(FULL),                      TG(FULL),            KC_6,           KC_7,           KC_8,           KC_9,      KC_0,       XXXX,
+  XXXX,    KC_Q,           KC_W,           KC_F,           KC_P,           KC_G,      XXXX,                      XXXX,            KC_J,           KC_L,           KC_U,           KC_Y,      KC_SCLN,    XXXX,
+  XXXX,    CMD_T(KC_A),    OPT_T(KC_R),    CTL_T(KC_S),    SFT_T(KC_T),    KC_D,      				 		  KC_H,    SFT_T(KC_N),    CTL_T(KC_E),    OPT_T(KC_I),  CMD_T(KC_O),    XXXX,
   XXXX,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,      LCTL(KC_SPC),              LCTL(KC_SPC),    KC_K,           KC_M,           KC_COMM,        KC_DOT,    KC_SLSH,    XXXX,
   XXXX,    XXXX,           XXXX,           XXXX,           KC_ESC,                                                                                KC_DEL,         XXXX,           XXXX,      XXXX,       XXXX,
-                                                                           XXXX,      XXXX,                      XXXX,            OSL(WIN),
+                                                                           XXXX,      XXXX,                      XXXX,            XXXX,
                                                                                       XXXX,                      XXXX,
                                            LT(NAVR, KC_BSPC), LT(FMDIA, KC_TAB),      XXXX,                      XXXX,            LT(SYMB, KC_ENT),   LT(NMBR, KC_SPC)
 ),
@@ -151,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+-----------|
  * |           |  [   |  7   |  8   |  9   |  ]   |      |           |      |      |      |      |      | Reset|           |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
- * |           |  ;   |  4   |  5   |  6   |  =   |------|           |------|      | SFT  | CTRL |  OPT | CMD  |           |
+ * |           |  '   |  4   |  5   |  6   |  =   |------|           |------|      | SFT  | CTRL |  OPT | CMD  |           |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
  * |           |  `   |  1   |  2   |  3   |  \   |      |           |      |      |      |      |      |      |           |
  * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
@@ -169,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
   XXXX,    XXXX,       XXXX,       XXXX,       XXXX,       XXXX,   XXXX,                    XXXX,        XXXX,       XXXX,          XXXX,         XXXX,          XXXX,           XXXX,
   XXXX,    KC_LBRC,    KC_7,       KC_8,       KC_9,       KC_RBRC,XXXX,                    XXXX,        XXXX,       XXXX,          XXXX,         XXXX,          RESET,          XXXX,
-  XXXX,    KC_SCLN,    KC_4,       KC_5,       KC_6,       KC_EQL,                                       XXXX,       KC_LSFT,       KC_LCTL,      KC_LOPT,       KC_LCMD,        XXXX,
+  XXXX,    KC_QUOT,    KC_4,       KC_5,       KC_6,       KC_EQL,                                       XXXX,       KC_LSFT,       KC_LCTL,      KC_LOPT,       KC_LCMD,        XXXX,
   XXXX,    KC_GRV,     KC_1,       KC_2,       KC_3,       KC_BSLS,XXXX,                    XXXX,        XXXX,       XXXX,          XXXX,         XXXX,          XXXX,           XXXX,
   XXXX,    XXXX,       XXXX,       XXXX,       KC_DOT,                                                               XXXX,          XXXX,         XXXX,          XXXX,           XXXX,
                                                            XXXX,   XXXX,                    XXXX,        XXXX,
@@ -184,7 +167,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+-----------|
  * |           |  {   |  &   |  *   |  (   |  }   |      |           |      |      |      |      |      | Reset|           |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
- * |           |  :   |  $   |  %   |  ^   |  +   |------|           |------|      | SFT  | CTRL |  OPT | CMD  |           |
+ * |           |  "   |  $   |  %   |  ^   |  +   |------|           |------|      | SFT  | CTRL |  OPT | CMD  |           |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
  * |           |  ~   |  !   |  @   |  #   |  |   |      |           |      |      |      |      |      |      |           |
  * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
@@ -202,46 +185,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
   XXXX,    XXXX,       XXXX,       XXXX,       XXXX,       XXXX,   XXXX,                    XXXX,        XXXX,       XXXX,          XXXX,         XXXX,          XXXX,           XXXX,
   XXXX,    KC_LCBR,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RCBR,XXXX,                    XXXX,        XXXX,       XXXX,          XXXX,         XXXX,          RESET,          XXXX,
-  XXXX,    KC_COLN,    KC_DLR,     KC_PERC,    KC_CIRC,    KC_PLUS,                                      XXXX,       KC_LSFT,       KC_LCTL,      KC_LOPT,       KC_LCMD,        XXXX,
+  XXXX,    KC_DQUO,    KC_DLR,     KC_PERC,    KC_CIRC,    KC_PLUS,                                      XXXX,       KC_LSFT,       KC_LCTL,      KC_LOPT,       KC_LCMD,        XXXX,
   XXXX,    KC_TILD,    KC_EXLM,    KC_AT,      KC_HASH,    KC_PIPE,XXXX,                    XXXX,        XXXX,       XXXX,          XXXX,         XXXX,          XXXX,           XXXX,
   XXXX,    XXXX,       XXXX,       XXXX,       KC_LPRN,                                                              XXXX,          XXXX,         XXXX,          XXXX,           XXXX,
                                                            XXXX,   XXXX,                    XXXX,        XXXX,
                                                                    XXXX,                    XXXX,
                                                KC_RPRN,    KC_UNDS,XXXX,                    XXXX,        ____,       XXXX
 ),
-
-/* Keymap 5: Windows layer
+/* Keymap 5: Full layer
  *
- * ,-----------------------------------------------------.           ,-----------------------------------------------------.
- * |           |      |      |      |      |      |      |           |      |      |      |      |      |      |           |
- * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+-----------|
- * |           |      |      |FulScn|      |      |PrvDis|           |NxtDis|   Up | Rght |      |      |      |           |
- * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
- * |           |AstMax|      |      |      |      |------|           |------|Maxim |      |      |      |      |           |
- * |-----------+------+------+------+------+------|Smalr |           |Largr |------+------+------+------+------+-----------|
- * |           |      |      | Cntr |      |      |      |           |      | Down | Left |      |      |      |           |
- * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
- *      |      |      |      |      |      |                                       |      |      |      |      |      |
- *      `----------------------------------'                                       `----------------------------------'
- *                                         ,-------------.           ,-------------.
- *                                         |      | LUC  |           | RUC  |      |
- *                                  ,------|------|------|           |------+------+------.
- *                                  |      |      | LDC  |           | RDC  |      |      |
- *                                  |      |      |------|           |------|      |      |
- *                                  |      |      |      |           |      |      |      |
- *                                  `--------------------'           `--------------------'
+ * ,-----------------------------------------------------.           ,----------------------------------------------------.
+ * | `         |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |  -       |
+ * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+----------|
+ * | =         |   Q  |   W  |   F  |   P  |   G  |      |           |      |   J  |   L  |   U  |   Y  |   ;  |  \       |
+ * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+----------|
+ * |           |   A  |   R  |   S  |   T  |   D  |      |           |      |   H  |   N  |   E  |   I  |   O  |  '       |
+ * |           |      |      |      |      |      |------|           |------|      |      |      |      |      |          |
+ * |-----------+------+------+------+------+------| tmux |           | tmux |------+------+------+------+------+----------|
+ * | SFT       |   Z  |   X  |   C  |   V  |   B  |      |           |      |   K  |   M  |   ,  |   .  |   /  |     SFT  |
+ * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+----------'
+ *   | CTL     | OPT  | CMD  |      | Esc  |                                       |  Del |      |  CMD |  OPT | CTL |
+ *   `-------------------------------------'                                       `------------------------------------'
+ *                                        ,-------------.       ,---------------.
+ *                                        |      |      |       |      |        |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      |      |       |      |        |      |
+ *                                 |Backsp| Tab  |------|       |------|  Enter |Space |
+ *                                 |      |      |      |       |      |        |      |
+ *                                 `--------------------'       `----------------------'
  */
-[WIN] = LAYOUT_ergodox_pretty(
+[FULL] = LAYOUT_ergodox_pretty(
   // left hand
-  XXXX,    XXXX,                      XXXX,    XXXX,                 XXXX,    XXXX,    XXXX,                   XXXX,                   XXXX,                  XXXX,                 XXXX,    XXXX,    XXXX,    XXXX,
-  XXXX,    XXXX,                      XXXX,    KC_WIN_FULLSCREEN,    XXXX,    XXXX,    KC_WIN_PREV_DISPLAY,    KC_WIN_NEXT_DISPLAY,    KC_WIN_BOTTOM_HALF,    KC_WIN_RIGHT_HALF,    XXXX,    XXXX,    XXXX,    XXXX,
-  XXXX,    KC_WIN_ALMOST_MAXIMIZE,    XXXX,    XXXX,                 XXXX,    XXXX,    KC_WIN_MAXIMIZE,        XXXX,                   XXXX,                  XXXX,                 XXXX,    XXXX,
-  XXXX,    XXXX,                      XXXX,    KC_WIN_CENTER,        XXXX,    XXXX,    KC_WIN_SMALLER,         KC_WIN_LARGER,          KC_WIN_TOP_HALF,       KC_WIN_LEFT_HALF,     XXXX,    XXXX,    XXXX,    XXXX,
-  XXXX,    XXXX,                      XXXX,    XXXX,                 XXXX,    XXXX,    XXXX,                   XXXX,                   XXXX,                  XXXX,
-                                               XXXX, KC_WIN_TOP_LEFT,      KC_WIN_TOP_RIGHT, XXXX,
-                                                        KC_WIN_BOTTOM_LEFT,      KC_WIN_BOTTOM_RIGHT,
-                                      XXXX, XXXX, XXXX,      XXXX, XXXX, XXXX
+  KC_GRV,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,      TG(FULL),                      TG(FULL),            KC_6,           KC_7,           KC_8,           KC_9,      KC_0,       KC_MINS,
+  KC_EQL,      KC_Q,           KC_W,           KC_F,           KC_P,           KC_G,      XXXX,                      XXXX,            KC_J,           KC_L,           KC_U,           KC_Y,      KC_SCLN,    KC_BSLS,
+  KC_ESC,      KC_A,           KC_R,           KC_S,           KC_T,           KC_D,                                                  KC_H,           KC_N,           KC_E,           KC_I,      KC_O,       KC_QUOT,
+  KC_LSFT,     KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,      LCTL(KC_SPC),              LCTL(KC_SPC),    KC_K,           KC_M,           KC_COMM,        KC_DOT,    KC_SLSH,    KC_RSFT,
+  KC_LCTL,     KC_LOPT,        KC_LCMD,        XXXX,           KC_ESC,                                                                                KC_DEL,         XXXX,           KC_RCMD,   KC_ROPT,    KC_RCTL,
+                                                                           XXXX,      XXXX,                      XXXX,            XXXX,
+                                                                                      XXXX,                      XXXX,
+                                           KC_BSPC, KC_TAB,      XXXX,                      XXXX,            KC_ENT,   KC_SPC
 ),
+
 };
 
 // Runs just one time when the keyboard initializes.
@@ -250,11 +234,6 @@ void matrix_init_user(void) {
   for (int i = LED_BRIGHTNESS_HI; i > LED_BRIGHTNESS_LO; i--) {
     ergodox_led_all_set (i);
     wait_ms (5);
-  }
-  wait_ms(1000);
-  for (int i = LED_BRIGHTNESS_LO; i > 0; i--) {
-    ergodox_led_all_set (i);
-    wait_ms (10);
   }
   ergodox_led_all_off();
 };
@@ -284,7 +263,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         ergodox_right_led_2_on();
         ergodox_right_led_3_on();
         break;
-      case WIN:
+      case FULL:
         ergodox_right_led_1_on();
         ergodox_right_led_2_on();
         ergodox_right_led_3_on();
